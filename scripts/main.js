6b72ee8d034d441c74eae9e6a1f5b5cd6251ad30
RobinPecheurTechnobel <134622479+RobinPecheurTechnobel@users.noreply.github.com>
2023-07-03 11:37:31 +0200

    mineur : gestion classement vide pour affichage

--- scripts/main.js
+++ scripts/main.js
@@ -17,23 +17,36 @@ function  goGame(difficulté){
 }
 
 function replissageClassment(){
-    let tableauClassement = document.getElementById("classement").getElementsByTagName("tbody")[0];
-    let classement = JSON.parse(localStorage.getItem("memory")).classement;
     
-    for (const valeur of classement) {
-        let tr = document.createElement("tr");
-        tableauClassement.appendChild(tr);
+    let memory = JSON.parse(localStorage.getItem("memory"))
+    
+    let tableauClassement = document.getElementById("classement");
+    if(!memory || !classement || classement.length ==0){
+        let zoneClassement = document.getElementById("zoneClassement");
+        zoneClassement.removeChild(tableauClassement);
         
-        let td1 = document.createElement("td");
-        tr.appendChild(td1);
-        td1.innerHTML = valeur.pseudo;
-        
-        let td2 = document.createElement("td");
-        tr.appendChild(td2);
-        td2.innerHTML = valeur.score;
+
+        let p = document.createElement("p");
+        zoneClassement.appendChild(p)
+        p.innerHTML = "Il n'y a pas encore de score enregistré.";
     }
-
-
+    else{
+        tbody = tableauClassement.getElementsByTagName("tbody")[0];
+        let classement = memory.classement;
+        
+        for (const valeur of classement) {
+            let tr = document.createElement("tr");
+            tableauClassement.appendChild(tr);
+            
+            let td1 = document.createElement("td");
+            tr.appendChild(td1);
+            td1.innerHTML = valeur.pseudo;
+            
+            let td2 = document.createElement("td");
+            tr.appendChild(td2);
+            td2.innerHTML = valeur.score;
+        }
+    }
 }
 
 replissageClassment();
